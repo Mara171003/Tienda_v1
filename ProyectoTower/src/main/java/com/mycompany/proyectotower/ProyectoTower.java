9f1e639cd7466975ef3628d18ec110228348f491
racatal <catvalra@gmail.com>
2023-04-21 23:42:52 -0600

    Prueba 2

--- ProyectoTower/src/main/java/com/mycompany/proyectotower/ProyectoTower.java
+++ ProyectoTower/src/main/java/com/mycompany/proyectotower/ProyectoTower.java
@@ -11,6 +11,6 @@ package com.mycompany.proyectotower;
 public class ProyectoTower {
 
     public static void main(String[] args) {
-        System.out.println("Hola!");
+        System.out.println("Hola! mundo");
     }
 }
